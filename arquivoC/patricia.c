c18e3efdbca55e6afeabf799f6d1a776c07765ca
João Bradachi <joao.bradachi@ufv.br>
2023-05-12 10:54:39 -0300

    patrícia de palavras

--- arquivoC/patricia.c
+++ arquivoC/patricia.c
@@ -1,2 +1,117 @@
+#include "../headers/patricia.h"
 
+TipoDib Bit(TipoIndexAmp i, TPalavra k){
+// Retorna o i-ésimo bit da chave k a partir da esquerda
+    return k.Palavra[i];
+}
+short EExterno (TipoArvore p){
+    // verifica se p é nó externo
+    return (p->nt == Externo);
+}
 
+TipoArvore CriaNoInt(int i, TipoArvore* Esq, TipoArvore* Dir, char letra){
+    TipoArvore p;
+
+    p = (TipoArvore) malloc(sizeof(TipoPatNo));
+
+    p->nt = Interno;
+    p->NO.NoInterno.Esq = *Esq;
+    p->NO.NoInterno.Dir = *Dir;
+    p->NO.NoInterno.Index = i;
+    p->NO.NoInterno.letra = letra;
+
+    return p;
+}
+
+TipoArvore CriaNoExt(TPalavra k, int IdDoc){
+    TipoArvore p;
+
+    p = (TipoArvore)malloc(sizeof(TipoPatNo));
+
+    p->nt = Externo;
+    strcpy(p->NO.tpalavra.Palavra, k.Palavra);
+    FLOcorrencias_Vazia(&(p->NO.tpalavra.ocorrencias));
+    Insere_Ocorrencia(&(p->NO.tpalavra.ocorrencias), IdDoc);
+
+    return p;
+
+}
+
+void Pesquisa (TPalavra k, TipoArvore t){
+    if(EExterno(t)){
+        if(!(strcmp(k.Palavra, t->NO.tpalavra.Palavra))){
+        printf("Elemento encontrado\n");
+        printf("%s, %d\n", t->NO.tpalavra.Palavra, t->NO.tpalavra.ocorrencias.pPrimeiro);
+        }
+        else{
+            printf("Elemento não encontrado\n");
+        }
+        return;
+    }
+    if(Bit(t->NO.NoInterno.Index, k) > t->NO.NoInterno.letra){
+        Pesquisa(k, t->NO.NoInterno.Dir);
+    }else{
+        Pesquisa(k, t->NO.NoInterno.Esq);
+    }
+}
+
+TipoArvore InsereEntre(TPalavra k, TipoArvore* t, int i, int IdDoc){
+    TipoArvore p;
+    if (EExterno(*t) || i < (*t)->NO.NoInterno.Index){
+        //Cria novo nó externo
+        p = CriaNoExt(k, IdDoc);
+        if(Bit(i, k) > (*t)->NO.NoInterno.letra){
+            return CriaNoInt(i, t, &p, Bit(i, k));
+        }else{
+            return CriaNoInt(i, &p, t, Bit(i, k));
+        }
+    }else{
+        if(Bit((*t)->NO.NoInterno.Index, k) > (*t)->NO.NoInterno.letra){
+            (*t)->NO.NoInterno.Dir = InsereEntre(k, &(*t)->NO.NoInterno.Dir, i, IdDoc);
+        }else{
+            (*t)->NO.NoInterno.Esq = InsereEntre(k, &(*t)->NO.NoInterno.Esq, i, IdDoc);
+        }
+        return (*t);
+    }
+}
+
+TipoArvore Insere(TPalavra k, TipoArvore* t, int IdDoc){
+    TipoArvore p;
+    int i;
+    if(*t == NULL){
+        return CriaNoExt(k, IdDoc);
+    }
+    else{
+        p = *t;
+        while(!EExterno(p)){
+            if(Bit(p->NO.NoInterno.Index, k) > p->NO.NoInterno.letra){
+                p = p->NO.NoInterno.Dir;
+            }
+            else{
+                p = p->NO.NoInterno.Esq;
+            }
+        }
+        // Encontra o primeiro bit diferente
+        i = 0;
+        while ((i<strlen(k.Palavra))& (Bit((int)i, k) == Bit((int)i, p->NO.tpalavra))){
+            i++;
+        }
+        if (!(strcmp(k.Palavra, p->NO.tpalavra.Palavra))){
+            // se tiver palavra, então adicionar ocorrencia
+            Insere_Ocorrencia(&(p->NO.tpalavra.ocorrencias), IdDoc);
+            return *t;
+        }
+        else{
+            return (InsereEntre(k, t, i, IdDoc));
+        }
+    }
+}
+
+void MostraArvore(TipoArvore t){
+    if(EExterno(t)){
+        printf("%s, %d\n", t->NO.tpalavra.Palavra, t->NO.tpalavra.ocorrencias.pPrimeiro);
+        return;
+    }
+    MostraArvore(t->NO.NoInterno.Esq);
+    MostraArvore(t->NO.NoInterno.Dir);
+}
--- headers/patricia.h
+++ headers/patricia.h
@@ -3,14 +3,15 @@
 typedef enum{
     Interno, Externo
 } TipoNo;
-
+typedef int TipoIndexAmp;
+typedef char TipoDib;
 typedef struct TipoPatNo* TipoArvore;
 typedef struct TipoPatNo{
     TipoNo nt;
     union{                          // Separa espaço para a maior estruturaem seu interior, variável ou é um ou outro
         struct{                     // mas com o mesmo espaço do maior entre eles 
-            char letra_no;
-            int index;
+            char letra;
+            TipoIndexAmp Index;
             TipoArvore Esq, Dir;
         }NoInterno;
 
@@ -18,6 +19,11 @@ typedef struct TipoPatNo{
     }NO;
 }TipoPatNo;
 
-void InsercaoPat(char* palavra, TipoArvore* t); // função de inserção
-TipoArvore CriaNoExt(char* palavra); // inserção auxiliar folha
-
+TipoDib Bit(TipoIndexAmp i, TPalavra k);
+TipoArvore CriaNoInt(int i, TipoArvore* Esq, TipoArvore* Dir, char letra);
+TipoArvore CriaNoExt(TPalavra k, int IdDoc);
+void Pesquisa (TPalavra k, TipoArvore t);
+TipoArvore InsereEntre(TPalavra k, TipoArvore* t, int i, int IdDoc);
+TipoArvore Insere(TPalavra k, TipoArvore* t, int IdDoc);
+short EExterno (TipoArvore p);
+void MostraArvore(TipoArvore t);
