66f030059bf15ad0486bee606fe386792450f8b2
Duds04 <mpbduda@gmail.com>
2023-05-16 13:00:03 -0300

    leitura1

--- arquivoC/leitura.c
+++ arquivoC/leitura.c
@@ -1,2 +1,32 @@
 #include "../headers/leitura.h"
 
+int leituraArquivo
+{
+    FILE *arquivo;
+    int idDoc = 1;
+    int x = 1;
+    char guardaPalavra[200]; // vetor auxiliar para guardar palavra
+
+    TipoArvore Pat = NULL;
+
+    pCelulaDicionario pAux;
+    pAux = pDicionario->pPrimeiro->pProx;
+
+    if ((arquivo = fopen(pTexto, "r")) != NULL)
+    {
+        while (fscanf(arquivo, "%s%c", guardaPalavra, &c) != EOF)
+        {
+            Pat = Insere(guardaPalavra, &Pat, idDoc);
+            if (c == '\n')
+            {
+                x++;
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Erro ao abrir o arquivo");
+            return 0;
+        }
+
+        fclose(arquivo);
+    }
